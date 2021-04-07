bd479b9dbdacedd48918441f1409f322c047da10
ekzemplaro <linux@hi-ho.ne.jp>
2021-04-07 16:08:30 +0900

    Commit on Apr/07/2020

--- go_upload.sh
+++ go_upload.sh
@@ -1,9 +1,9 @@
 #
-sudo ufw default allow outgoing
+#sudo ufw default allow outgoing
 #
 git add --all
-git commit -m "Commit on Nov/27/2019"
+git commit -m "Commit on Apr/07/2020"
 git push -u origin master
 #
-sudo ufw default deny outgoing
+#sudo ufw default deny outgoing
 #
